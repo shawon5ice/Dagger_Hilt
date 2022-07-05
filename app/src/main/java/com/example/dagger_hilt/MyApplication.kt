7eab4925809b7690e801ee970c44da93ba2caa69
shawon <shawon5ice@gmail.com>
2022-07-05 17:23:06 +0600

    initial commit

--- app/src/main/java/com/example/dagger_hilt/MyApplication.kt
+++ app/src/main/java/com/example/dagger_hilt/MyApplication.kt
@@ -1,4 +1,9 @@
 package com.example.dagger_hilt
 
-class MyApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
 }
---
+++ app/src/main/java/com/example/dagger_hilt/modules/AppModule.kt
@@ -0,0 +1,17 @@
+package com.example.dagger_hilt.modules
+
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideString() = "This is the string we will inject"
+}
